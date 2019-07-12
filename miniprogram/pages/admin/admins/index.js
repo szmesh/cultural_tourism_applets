29165d6154fe1c8ed80ae41f5860781c73b3b857
Salmon S M YANG <49427823@163.com>
2019-07-12 23:16:02 +0800

    Feature: 管理员列表模式和管理员判断

--- miniprogram/pages/admin/admins/index.js
+++ miniprogram/pages/admin/admins/index.js
@@ -4,8 +4,8 @@ Page({
   data: {
     admins_table_view: 'mcta_admins',
     users_table_view: 'mcta_users',
-    userInfo: {},
-    commetatorModel: {}
+    usersDataSource: [],
+    adminsDataSource: []
   },
 
   onLoad: function () {
@@ -16,7 +16,89 @@ Page({
         userInfo: app.globalData.userInfo
       })
 
-      
+      // 获取所有用户
+      this.getUsersDataSource()
     }
   },
+
+  getUsersDataSource: function() {
+    const db = wx.cloud.database()
+    let _this = this
+    db.collection(_this.data.users_table_view).get({
+      success: res => {
+        if (0 < res.data.length) {
+          this.setData({
+            usersDataSource: res.data
+          })
+
+          // 获取所有管理员
+          this.getAdminsDataSource()
+        } else {
+          this.setData({
+            usersDataSource: []
+          })
+        }
+      },
+      fail: err => {
+        this.setData({
+          usersDataSource: []
+        })
+      }
+    })
+  },
+
+  getAdminsDataSource: function() {
+    const db = wx.cloud.database()
+    let _this = this
+    db.collection(_this.data.admins_table_view).get({
+      success: res => {
+        if (0 < res.data.length) {
+          _this.setData({
+            adminsDataSource: res.data
+          })
+
+          // 根据管理员列表重置用户列表
+          _this.resetUsersDataSourceWithAdmins()
+        } else {
+          _this.setData({
+            adminsDataSource: []
+          })
+        }
+      },
+      fail: err => {
+        _this.setData({
+          adminsDataSource: []
+        })
+      }
+    })
+  },
+
+  resetUsersDataSourceWithAdmins: function() {
+    let usersDataSource = this.data.usersDataSource
+    for (let i = 0; i < usersDataSource.length; i++) {
+        let user = usersDataSource[i]
+      let adminsFilter = this.data.adminsDataSource.filter(item => item._openid == user._openid)
+      if(undefined == adminsFilter || 0 >= adminsFilter.length) {
+        continue
+      }
+
+      user.adminIndex = i
+      user.adminId = adminsFilter[i]._id
+      usersDataSource[i] = user
+    }
+
+    this.setData({
+      usersDataSource: usersDataSource
+    })
+  },
+
+  // 根据用户的openid查找存在的管理员对象
+  getAdminModelWithOpenId: function(openId) {
+    let res = this.data.adminsDataSource.filter(item => item._openid == openid)
+    if (undefined == res || 0 >= res.length) {
+      return undefined
+    }
+
+    return res[0]
+  }
 })
